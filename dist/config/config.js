aa0ab91cef7b07c8fac7ad5c881592e7c1ac5c5c
DaniloDeV90 <duhflores78@gmail.com>
2023-08-16 16:03:15 -0300

    eu na guento

---
+++ dist/config/config.js
@@ -0,0 +1,19 @@
+"use strict";require ("dotenv").config ();
+
+
+module.exports = {
+    database:"Escola",
+    username:"root",
+    password: "dfgdf11234",
+    host: "localhost",
+    dialect: "mariadb",
+    port: 3306,
+    database:"Escola",
+    define: {
+        timeStamps: true
+    },
+    dialectOptions: {
+        connectTimeout: 60000
+      },
+}
+
