08e5294be4eef0ffbd71a2db0ecef536ad6f365d
DaniloDeV90 <duhflores78@gmail.com>
2023-08-16 04:11:55 -0300

    sad

--- dist/config/config.js
+++ dist/config/config.js
@@ -2,8 +2,10 @@
 
 
 module.exports = {
-    dialect: process.env.DATABASE_NAME_DATA,
+
     host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_NAME_DATA,
+  
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
