8168ce24e5099f7e0eed8340cb317d6da908d844
DaniloDeV90 <duhflores78@gmail.com>
2023-08-16 13:54:05 -0300

    ksad

--- src/config/config.js
+++ src/config/config.js
@@ -3,13 +3,14 @@ require ("dotenv").config ();
 
 module.exports = {
 
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_NAME_DATA,
+   
+    host: "localhost",
+    dialect: "mariadb",
   
-    port: process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    port: 3001,
+    username:"root",
+    password: "dfgdf11234",
+    database:Escola,
     define: {
         timesStamps: true
     },
