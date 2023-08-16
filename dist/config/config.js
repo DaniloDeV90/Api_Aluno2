76e1f0beee3c413b14415b4a4385cd2a66025c79
DaniloDeV90 <duhflores78@gmail.com>
2023-08-16 15:19:14 -0300

    sd

--- dist/config/config.js
+++ dist/config/config.js
@@ -2,17 +2,14 @@
 
 
 module.exports = {
-
-   
-    host: "localhost",
+    host: "130.211.230.172",
     dialect: "mariadb",
-  
     port: 3306,
     username:"root",
     password: "dfgdf11234",
     database:"Escola",
     define: {
-        timesStamps: true
+        timeStamps: true
     },
     dialectOptions: {
         connectTimeout: 60000
