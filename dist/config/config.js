0bd447f08279f2d215d955294193499d8e771b71
DaniloDeV90 <duhflores78@gmail.com>
2023-08-16 03:50:08 -0300

    feito

--- dist/config/config.js
+++ dist/config/config.js
@@ -2,14 +2,14 @@
 
 
 module.exports = {
-    dialect: process.env.DATABASE_NAME_DATA,
+    dialect: "mariadb",
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     define: {
-        timesStamps: true
+        timestamps: true
     },
     dialectOptions: {
         connectTimeout: 60000
--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,14 @@ require ("dotenv").config ();
 
 
 module.exports = {
-    dialect: "mariadb",
+    dialect: process.env.DATABASE_NAME_DATA,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     define: {
-        timestamps: true
+        timesStamps: true
     },
     dialectOptions: {
         connectTimeout: 60000
